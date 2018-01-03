fcdf8434169d8ebe6adc23754249d78d1a3188e6
Davanon <davanon2014@gmail.com>
2018-01-03 09:34:24 +0530

    First

---
+++ src/main/groovy/com/iqmsoft/demo/DemoApp.groovy
@@ -0,0 +1,17 @@
+package com.iqmsoft.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+
+
+@SpringBootApplication
+class DemoApp {
+
+    static void main(String[] args) {
+        SpringApplication.run DemoApp, args
+    }
+
+	
+    
+}
